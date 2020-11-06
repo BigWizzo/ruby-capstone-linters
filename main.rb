248db9542e0eda4d1782643a023c2a8c2b34dac5
bigwizzo <wnnwillard@gmail.com>
2020-11-06 19:20:03 +0200

    Add logic for catching mismatching brackets

--- main.rb
+++ main.rb
@@ -12,29 +12,45 @@ end
 
 trailing_white_space(file_array)
 
-# brackets into an array
-def brackets_array(arr, bra_arr)
-  i = 0
-  while i < arr.length
-    chec = arr[i].scan(/\{|\}/)
-    if chec == ['{']
-      bra_arr.push('{')
-    elsif chec == ['}']
-      bra_arr.push('}')
-    end
-    i += 1
+file_array.each_with_index do |line_string, index|
+  chec = line_string.scan(/\{|\}/)
+  "#{chec} => #{index + 1}"
+  your_hash = {}
+  if chec == ['{']
+    bra_arr.push('{')
+  elsif chec == ['}']
+    bra_arr.push('}')
   end
-  bra_arr
+  # puts your_hash
 end
+# p bra_arr
+your_hash = {}
 
-brackets_array(file_array, bra_arr)
-
-bra_string = bra_arr.join('')
-brackets_array(file_array, bra_arr)
-bra_arr.each do |x|
-  # p bra_string
-  p bra_string =~ /\{(?=\})/
+# return hash of bracets
+file_array.each_with_index do |line_string, index|
+  chec = line_string.scan(/\{|\}/)
+    if chec == ['{']
+      your_hash[index + 1] = '{'
+    elsif chec == ['}']
+      your_hash[index + 1] = '}'
+    end
 end
+# puts your_hash
 
-# 
+#  your_hash_arr_values = your_hash.values
+#  your_hash_arr_keys = your_hash.keys
 
+ hash_arr = your_hash.to_a
+ print hash_arr
+
+ i = 0
+ while i < hash_arr.length
+  j = 1
+  while j < hash_arr[i].length
+    if !hash_arr[i + 1].nil? && hash_arr[i][1] == hash_arr[i + 1][1]
+     print "No matching bracket '#{hash_arr[i][1]}': line #{hash_arr[i + 1][0]}\n"
+    end
+    j += 1
+  end
+  i += 1
+ end
