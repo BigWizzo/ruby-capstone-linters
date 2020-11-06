9697c8aa94d04f5b485be6fe53ec544a823e2495
bigwizzo <wnnwillard@gmail.com>
2020-11-06 20:20:58 +0200

    Initialize the LintProcess class

--- temp.rb
+++ temp.rb
@@ -1,17 +1,6 @@
-class LintProcess
-  attr_reader :file, :file_array, :hash_arr, :bracket_hash
-
-  def initialize
-    @file = 'lorem.css'
-    @file_array = File.readlines(file)
-    @bracket_hash = {}
-    @hash_arr = []
-  end
-  
-end
-
 file='lorem.css'
 file_array = File.readlines(file)
+bra_arr = []
 bracket_hash = {}
 hash_arr = []
 
