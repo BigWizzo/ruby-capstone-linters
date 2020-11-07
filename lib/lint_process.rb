08ef4c4514ffa8f74e8da81c84e8255dae4d29ed
bigwizzo <wnnwillard@gmail.com>
2020-11-07 05:02:59 +0200

    Fix errors with rubocop

--- lib/lint_process.rb
+++ lib/lint_process.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+# LintProces class initializing the file
 class LintProcess
-    attr_reader :file, :file_array, :hash_arr, :bracket_hash
-  
-    def initialize
-      @file = 'lorem.css'
-      @file_array = File.readlines(file)
-      @bracket_hash = {}
-      @hash_arr = []
-    end
+  attr_reader :file, :file_array, :hash_arr, :bracket_hash
+
+  def initialize
+    @file = 'lorem.css'
+    @file_array = File.readlines(file)
+    @bracket_hash = {}
+    @hash_arr = []
   end
+end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require_relative '../bin/main'
 
 RSpec.describe 'An idial CSS Linter' do
-  let(:linter) {LintProcess.new }
-  let(:file_array) { ['the{', 'file}', 'the}', 'file}', 'maybe'] }
+  let(:linter) { LintProcess.new }
+  let(:file_array) { ['the{', 'file}', 'maybe'] }
   let(:bracket_hash) { {} }
-  let(:hash_arr) {  [[1, "{"], [2, "}"], [3, "}"], [4, "}"]] }
+  let(:hash_arr) { [[1, '{'], [2, '}']] }
   describe '#bracket_line' do
     it 'Returns a hash' do
-      expect(bracket_line(file_array, bracket_hash)).to eq({1=>"{", 2=>"}", 3=>"}", 4=>"}"})
+      expect(bracket_line(file_array, bracket_hash)).to eq({ 1 => '{', 2 => '}' })
     end
   end
   describe '#check_bracket' do
