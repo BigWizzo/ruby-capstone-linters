8eee6281e854dd064daa61fc11a19581569cba98
bigwizzo <wnnwillard@gmail.com>
2020-11-08 00:30:18 +0200

    Store errors into the @errors Hash

--- lib/lint_process.rb
+++ lib/lint_process.rb
@@ -6,6 +6,7 @@ class LintProcess
     @file = 'lorem.css'
     @file_array = File.readlines(file)
     @bracket_hash = {}
+    @errors = {}
   end
 
   def bracket_line
@@ -22,35 +23,38 @@ class LintProcess
     
   def check_bracket
     hash_arr = @bracket_hash.to_a
+    p hash_arr
     i = 0
     while i < hash_arr.length
       j = 1
       while j < hash_arr[i].length
         if !hash_arr[i + 1].nil? && hash_arr[i][1] == hash_arr[i + 1][1]
-          puts "No matching bracket '#{hash_arr[i][1]}' : line #{hash_arr[i + 1][0]}\n"
+          @errors.store("#{hash_arr[i + 1][0]}", "No matching bracket '#{hash_arr[i][1]}")
         end
         j += 1
       end
       i += 1
     end
+    @errors
   end
 
   def check_error
     file_array.each_with_index do |line_string, index|
       if line_string[-2] == ' '
-        puts "Trailing whitespace detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Trailing whitespace detected")
       elsif line_string.scan(/\{|\}/).length > 1
-        puts "Multiple brackets detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Multiple brackets detected")
       elsif line_string.scan(/\;/).length == 1 && line_string.strip[-1] != ';'
-        puts "Semicolon error detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Semicolon error detected")
       elsif line_string.scan(/\{/).length == 1 && line_string.strip[-1] != '{'
-        puts "Opening Bracket error detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Opening Bracket error detected")
       elsif line_string.scan(/\}/).length == 1 && line_string.strip[0] != '}'
-        puts "Closing Bracket error detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Closing Bracket error detected")
       elsif line_string.scan(/\;/).length > 1
-        puts "Multiple semi-colons detected: line #{index + 1}"
+        @errors.store("#{index + 1}", "Multiple semi-colons detected")
       end
     end
+    @errors
   end
 end
 
