5e6fc1043fea7b91806211124aed6b86c993b27c
bigwizzo <wnnwillard@gmail.com>
2020-11-09 05:54:05 +0200

    Fix RSpec

--- spec/lint_process_spec.rb
+++ spec/lint_process_spec.rb
@@ -1,29 +1,29 @@
 require_relative '../lib/lint_process'
 
 RSpec.describe 'An idial CSS Linter' do
-  let(:err_file) { LintProcess.new('err_file.css') }
-  let(:clean_file) { LintProcess.new('clean.css') }
-  let(:empty_file) { LintProcess.new('empty.css') }
+  let(:err_file) { LintProcess.new('./css/err_file.css') }
+  let(:clean_file) { LintProcess.new('./css/clean.css') }
+  let(:empty_file) { LintProcess.new('./css/empty.css') }
   describe 'The LintProcess class ' do
     describe '#bracket_line' do
       context 'when brackets are found in the file' do
         it 'Returns a hash of opening brackets and closing brackets' do
-          expect(err_file.bracket_line).to eq({
-                                                1 => '{',
-                                                3 => '}',
-                                                7 => '}',
-                                                9 => '{',
-                                                11 => '{',
-                                                12 => '}',
-                                                14 => '{',
-                                                16 => '}'
-                                              })
+          expect(err_file.send(:bracket_line)).to eq({
+                                                    1=>"}",
+                                                    3=>"{",
+                                                    4=>"}",
+                                                    8=>"}",
+                                                    10=>"{",
+                                                    12=>"}",
+                                                    14=>"{",
+                                                    16=>"}"
+                                                            })
         end
       end
 
       context 'when brackets are not found in the file' do
         it 'Returns an empty hash' do
-          expect(empty_file.bracket_line).to eq({})
+          expect(empty_file.send(:bracket_line)).to eq({})
         end
       end
     end
@@ -47,13 +47,15 @@ RSpec.describe 'An idial CSS Linter' do
     describe '#check_error' do
       context 'when errors are found' do
         it 'Returns a hash of errors' do
-          expect(err_file.check_error).to
-          eq({ '10' => 'Semicolon error detected',
-               '16' => 'Closing Bracket error detected',
-               '2' => 'Multiple semi-colons detected',
-               '4' => 'Trailing whitespace detected',
-               '5' => 'Multiple brackets detected',
-               '9' => 'Opening Bracket error detected' })
+          expect(err_file.check_error).to eq({
+            "1" => "Closing Bracket error detected",
+            "10" => "Opening Bracket error detected",
+            "11" => "Semicolon error detected",
+            "16" => "Closing Bracket error detected",
+            "2" => "Multiple semi-colons detected",
+            "5" => "Trailing whitespace detected",
+            "6" => "Multiple brackets detected",
+            })
         end
       end
 
